6ad6755093ac5d1e9d6dbee7c48e425d62f55a4e
Amanuel Tesfaye <amanueltsfy@gmail.com>
2022-12-16 10:49:25 +0300

    test task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error from './components/error';
+import Header from './components/header';
+import Schedule from './components/schedule';
+import ShowDetail from './components/schedule/showDetail';
+import NotFound from './components/_404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-full sm:h-screen flex flex-col px-8 sm:px-16 py-6 sm:py-12 relative'>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Schedule />} />
+          <Route path='/:id' element={<ShowDetail />} />
+          <Route path='/error' element={<Error />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <div className='absolute top-0 left-0 w-full h-1/2 bg-gray-200 z-0'></div>
     </div>
   );
 }
